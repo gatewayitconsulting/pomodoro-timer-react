401ac9df78ee637d61e5b3916b70594865a9b474
gatewayitconsultingDesktop <43389272+gatewayitconsulting@users.noreply.github.com>
2020-06-17 13:08:04 -0500

    Implement Material Design Bootstrap

---
+++ pomodoro-timer/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import Timer from './components/Timer';
+import './App.css';
+import './assets/css/material-kit.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Timer />
+      </main>
+    </div>
+  );
+}
+
+export default App;
